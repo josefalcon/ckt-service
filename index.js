aae6a4b9759169fa9c6fe878b397db7eafa2cad3
Jose Falcon <falcon@spotify.com>
2016-09-13 23:19:28 -0400

    Add GET

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var gcs = gcloud.storage();
 var bucket = gcs.bucket('ckt');
 
 app.post('/aac', (req, res) => {
-  debug('POST /acc');
+  debug('POST /aac');
   var id = uuid.v4();
   var remoteWriteStream =
     bucket.file(id).save(JSON.stringify(req.body), function(err) {
@@ -26,6 +26,17 @@ app.post('/aac', (req, res) => {
       }
       return res.status(500).json({ error: err });
     });
+});
+
+app.get('/aac/:id', (req, res) => {
+  var id = req.query.id;
+  if (!id) {
+    return res.status(404);
+  }
+
+  debug('GET /aac', id);
+  res.set('Content-Type', 'application/json');
+  res.send(bucket.file(id).createReadStream());
 });
 
 var port = process.env.PORT || 3000;
